c1ec80dcb4392d59856a676b91af790229387fe0
Tengku Hafidz <tengkuhafidz@gmail.com>
2022-12-29 14:13:58 +0800

    update copy

--- utils/budget.ts
+++ utils/budget.ts
@@ -4,7 +4,7 @@ import { DbQueries } from "../db-queries/index.ts";
 
 export const displayNoExistingBudget = (ctx: Context) => {
     const noBudgetExistText = `<b>No Existing Budget 😅</b>
-Add budget to be tracked with /${BotCommands.Add}`
+Add new budget with /${BotCommands.Add}`
     ctx.reply(noBudgetExistText, {
         parse_mode: "HTML",
     });
